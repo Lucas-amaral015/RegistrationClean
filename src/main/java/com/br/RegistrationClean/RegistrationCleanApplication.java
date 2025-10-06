b59c05e94ec9a3cb7351710ac202096c640c431e
Lucas Amaral <Lucasam@outlook.com>
2025-10-06 16:12:23 -0300

    first commit

---
+++ src/main/java/com/br/RegistrationClean/RegistrationCleanApplication.java
@@ -0,0 +1,13 @@
+package com.br.RegistrationClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/RegistrationClean/RegistrationCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.RegistrationClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
